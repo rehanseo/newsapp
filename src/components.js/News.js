5c3d06b1a7b5a560b77551b89ed7942962e56b61
rehan <rehan.saeed101@gmail.com>
2024-04-02 23:07:54 +0500

    New launch news wesite

---
+++ src/components.js/News.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>This is a news components
+        <NewsItems/>
+        
+      </div>
+    )
+  }
+}
